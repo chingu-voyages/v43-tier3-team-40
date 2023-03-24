da831e17ca27167895182cd613ab242e098d77b6
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-03-24 11:55:17 -0700

    Install react with TSX in client folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
-export const App = () => {
-	return (
-		<div>
-			<h1 className="text-2xl text-red-700">Fitness tracker</h1>
-		</div>
-	);
-};
+
+import { useState, useEffect } from "react";
+
+function App(): JSX.Element {
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+
+export default App;
