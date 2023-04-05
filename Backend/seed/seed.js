60f2dd12d7e71a3bf62f12b36f347c69194b9272
Connor Wales <connorwales@gmail.com>
2023-04-04 18:50:55 -0700

    finished seed file, updated readme, installed uuidv4 and bcrypt, created models folder in Backend

--- Backend/seed/seed.js
+++ Backend/seed/seed.js
@@ -1,10 +1,11 @@
-const db = require('../db');
 
 /** One function to seed whichever database is being
  * pointed to by process.env.MODE. 
- */
+*/
 
 const seed = async() => {
+  
+  const db = require('../db');
 
   db.on('connect', async () => {
     // console.log(db.database)
@@ -12,11 +13,11 @@ const seed = async() => {
 
     // clear all tables
     await db.query('DROP TABLE IF EXISTS users CASCADE');
-    await db.query('DROP TABLE IF EXISTS users CASCADE');
-    await db.query('DROP TABLE IF EXISTS users CASCADE');
-    await db.query('DROP TABLE IF EXISTS users CASCADE');
-    await db.query('DROP TABLE IF EXISTS users CASCADE');
-    await db.query('DROP TABLE IF EXISTS users CASCADE');
+    await db.query('DROP TABLE IF EXISTS user_profiles CASCADE');
+    await db.query('DROP TABLE IF EXISTS days CASCADE');
+    await db.query('DROP TABLE IF EXISTS activities CASCADE');
+    await db.query('DROP TABLE IF EXISTS meals CASCADE');
+    await db.query('DROP TABLE IF EXISTS sleeps CASCADE');
 
     // users table
     /**
@@ -24,7 +25,7 @@ const seed = async() => {
      * anyone from inferring the number of users
      */
     await db.query(`CREATE TABLE users(
-      user_id VARCHAR UNIQUE PRIMARY_KEY,
+      id VARCHAR PRIMARY KEY,
       username VARCHAR UNIQUE,
       email VARCHAR UNIQUE,
       hashed_password VARCHAR,
@@ -36,36 +37,88 @@ const seed = async() => {
       );`
     )
 
-    // user_profiles table
-    await db.query(`CREATE TABLE user_profiles(
+    console.log('created table users');
 
+    // user_profiles table
+    /**
+     * note: the avatar is currently a varchar so as 
+     * to be used as a pointer to some remote image 
+     * repository, though this may be modified later
+     * to a BLOb or some other direct storage if
+     * possible
+     */
+    await db.query(`CREATE TABLE user_profiles(
+      id INTEGER PRIMARY KEY,
+      user_id VARCHAR REFERENCES users (id),
+      avatar VARCHAR,
+      height INTEGER,
+      weight INTEGER,
+      age INTEGER
       );`
     )
+
+    console.log('created table user_profiles');
 
     // days table
     await db.query(`CREATE TABLE days(
-
+      id INTEGER PRIMARY KEY,
+      user_id VARCHAR REFERENCES users (id)
       );`
     )
+
+    console.log('created table days');
 
     // activities table
     await db.query(`CREATE TABLE activities(
+      id INTEGER PRIMARY KEY,
+      day_id INTEGER REFERENCES days (id),
+      category VARCHAR,
+      start_time TIMESTAMPTZ,
+      end_time TIMESTAMPTZ,
+      intensity INTEGER,
+      success_rating INTEGER,
 
+      CONSTRAINT intensity_scale CHECK (
+        intensity >=1 AND intensity <= 10
+        ),
+      CONSTRAINT success_rating_scale CHECK (
+        success_rating >=1 AND success_rating <= 10
+        )
       );`
     )
+
+    console.log('created table activities');
 
     // meals table
     await db.query(`CREATE TABLE meals(
-
+      id INTEGER PRIMARY KEY,
+      day_id INTEGER REFERENCES days(id),
+      calories INTEGER,
+      carbs INTEGER,
+      protein INTEGER,
+      fat INTEGER,
+      dietary_restrictions VARCHAR,
+      time TIMESTAMPTZ
       );`
     )
 
+    console.log('created table meals');
 
     // sleeps table
     await db.query(`CREATE TABLE sleeps(
+      id INTEGER PRIMARY KEY,
+      day_id INTEGER REFERENCES days (id),
+      start_time TIMESTAMPTZ,
+      end_time TIMESTAMPTZ,
+      success_rating INTEGER,
 
+      CONSTRAINT success_rating_scale CHECK (
+        success_rating >=1 AND success_rating <= 10
+        )
       );`
     )
+
+    console.log('created table sleeps');
 
   })
 }
--- README.md
+++ README.md
@@ -33,3 +33,7 @@ Add-project-description-here
 #### DB Creation
 1. In terminal, type `createdb bodybalance`
 2. Then type `createdb bodybalance_test`
+3. To seed, type `node` into the terminal to enter the NodeJS console
+4. Type `const seed = require('./seed/seed');` to import seed
+5. Type `seed()` to run the seed function. You should see in the log as all of the tables are created. This will seed the test table
+6. To seed the production table, repeat this process, but type `process.env.mode="production";` before importing seed.
