fdf2deba77a97770970af49e6f36a192aad58a8a
Connor Wales <connorwales@gmail.com>
2023-04-06 17:29:05 -0700

    installed nodemon, jsonwebtoken, set up basic middleware and error handler in server, got users/createUser working though no integration tests yet

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,43 @@
 const express = require('express')
 
+const usersRouter = require('./routes/users');
+const { NotFoundError } = require('./expressError');
+
 // App
-const app = express()
+const app = express();
+
+// middleware
+app.use(express.json());
+
+
+// routes
+app.use('/users', usersRouter);
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+// 404 error handler
+app.use((req, res, next) => {
+  return next(new NotFoundError())
+})
+
+
+// error handler
+app.use((err, req, res, next) => {
+
+  console.log(err);
+  
+  // Any ExpressError will pass along its own error status
+  const status = err.status_code || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status }
+  });
+
+})
+
+
 const port = 3000
 app.listen(port, () => console.log(`App listening on port ${port}!`))
