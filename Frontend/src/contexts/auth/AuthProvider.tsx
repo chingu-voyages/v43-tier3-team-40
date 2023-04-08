34f07a2ef2ef2512ec67367a5f6b811a334bbe00
JaacielBriseo <jaacielbris23@gmail.com>
2023-04-07 23:58:40 -0700

    User authentication methods finish
    Users can register, login and persist the login until they explicitly make the logout

--- Frontend/src/contexts/auth/AuthProvider.tsx
+++ Frontend/src/contexts/auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useReducer } from 'react';
+import { PropsWithChildren, useCallback, useEffect, useReducer } from 'react';
 import { authApi } from '../../api';
 import { IUser } from '../../interfaces';
 import { AuthContext, authReducer } from './';
@@ -6,7 +6,7 @@ export interface AuthState {
 	status: 'authenticated' | 'not-authenticated' | 'checking';
 	user?: IUser;
 }
-interface RegisterResponse extends IUser {
+interface AuthResponse extends IUser {
 	token: string;
 }
 
@@ -17,26 +17,24 @@ const Auth_INITIAL_STATE: AuthState = {
 export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
 	const [state, dispatch] = useReducer(authReducer, Auth_INITIAL_STATE);
 
-	// startLogin function should call the login endpoint and the backend should
-	// return a user with just the email and name in case that the credentials are correct.
-	// For the moment i'm using the password as the name just for testing
-	const startLogin = (email: string, password: string) => {
+	const startLogin = async (username: string, password: string): Promise<void> => {
 		dispatch({ type: '[Auth] - Checking credentials' });
-
-		//TODO:
-		//! Make this function async
-		//! Make a try/catch calling the login endpoint,
-		//! Try the login and if everything is correct use the data coming from the backend to dispatch login.
-		//! If credentials are bad or something is wrong from the backend, dispatch the logout.
-
-		setTimeout(() => {
-			dispatch({ type: '[Auth] - Login', payload: { email, username: password } });
-		}, 2000);
+		try {
+			const { data } = await authApi.post<AuthResponse>('/login', { username, password });
+			const { token } = data;
+			localStorage.setItem('token', token);
+			dispatch({ type: '[Auth] - Login', payload: { email:data.email, username } });
+		} catch (error) {
+			console.error(error);
+			//We should create a modal or a popover to provide the user information if something goes wrong.
+			dispatch({ type: '[Auth] - Logout' });
+		}
 	};
+
 	const startRegister = async (username: string, email: string, password: string): Promise<void> => {
 		dispatch({ type: '[Auth] - Checking credentials' });
 		try {
-			const { data } = await authApi.post<RegisterResponse>('/createUser', { username, email, password });
+			const { data } = await authApi.post<AuthResponse>('/createUser', { username, email, password });
 			const { token } = data;
 			localStorage.setItem('token', token);
 			dispatch({ type: '[Auth] - Login', payload: { email, username } });
@@ -47,7 +45,31 @@ export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
 		}
 	};
 
-	const logout = () => dispatch({ type: '[Auth] - Logout' });
+	const checkAuthToken = useCallback(async () => {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			dispatch({ type: '[Auth] - Logout' });
+			return;
+		}
+		try {
+			const { data } = await authApi.get('/renew');
+			const { username, email } = data;
+			localStorage.setItem('token', data.token);
+			dispatch({ type: '[Auth] - Login', payload: { email, username } });
+		} catch (error) {
+			console.error(error);
+			dispatch({ type: '[Auth] - Logout' });
+		}
+	}, []);
+
+	const logout = () => {
+		localStorage.clear();
+		dispatch({ type: '[Auth] - Logout' });
+	};
+
+	useEffect(() => {
+		checkAuthToken();
+	}, []);
 	return (
 		<AuthContext.Provider
 			value={{
