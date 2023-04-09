5d2708c3ca53fadc46fea754354148a1c4ccef9b
Connor Wales <connorwales@gmail.com>
2023-04-08 21:17:14 -0700

    finished UserProfile.editUserProfile and PUT user/editUser route, appears to be working but needs testing

--- Backend/models/UserProfile.js
+++ Backend/models/UserProfile.js
@@ -3,6 +3,8 @@
 
 // local
 const db = require('../db.js');
+const dynamicUpdateQuery = require('../helpers/dynamicUpdateQuery.js');
+const { InvalidUserError } = require('../expressError.js');
 
 
 const getUserProfileByUserId = async (user_id) => {
@@ -20,11 +22,29 @@ module.exports.getUserProfileByUserId = getUserProfileByUserId;
 
 
 
-
-const editUserProfile = async () => {
+/** Takes request body and user_id, and updates the user_profile with a
+ * sanitized version of the body. Returns the updated UserProfile.
+ * 
+ * 
+ * @param {Obect} body
+ * @param {uuid} user_id 
+ * @returns {UserProfile}
+ */
+const editUserProfile = async (body, user_id) => {
   try {
-
+    // filter body to only get appropriate keys
+    const updateObj = {
+      avatar: body.avatar ? body.avatar : null,
+      height: body.height ? body.height : null,
+      weight: body.weight ? body.weight : null,
+      age: body.age ? body.age : null
+    }
+    const queryString = dynamicUpdateQuery(updateObj, 'user_profiles', 'user_id', user_id);
+    const updateQuery = await db.query(queryString);
+    if (updateQuery.rows.length !== 1) throw new InvalidUserError();
+    else return updateQuery.rows[0];
   } catch(err) {
     throw err;
   }
 }
+module.exports.editUserProfile = editUserProfile;
